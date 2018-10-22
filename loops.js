25c2a24f9cbbf1de12781abca15d1935642e723a
Kah Yap <kahyap13@gmail.com>
2018-10-22 12:53:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,19 +9,3 @@ function forLoop(array) {
   return array;
 }
 
-function whileLoop(n) {
-  while(n > 0) {
-    console.log(n--)
-  }
-  return "done"
-}
-
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
-function doWhileLoop(array) {
-  do {
-    array.pop()
-  } while(array.length > 0 || maybeTrue())
-  return array;
